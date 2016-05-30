4fec4906616e68150b140540e0dc21a1461b4c20
lazerzes <lazerzes@gmail.com>
2016-05-30 14:07:25 -0400

    Version Number Fix

--- src/main/java/com/lazerzes/anvils/library/MiscLib.java
+++ src/main/java/com/lazerzes/anvils/library/MiscLib.java
@@ -5,6 +5,6 @@ public class MiscLib {
 
     public static final String MOD_ID = "lazAnvils";
     public static final String MOD_NAME = "Anvil Enchantments";
-    public static final String MOD_VERSION = "1.0.1.0";
+    public static final String MOD_VERSION = "1.0.1.1";
 
 }
