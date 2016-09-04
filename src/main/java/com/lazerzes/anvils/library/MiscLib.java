5c255762ab05e16fc6fdfe498055cb0a525591f1
Sam <samson.bassett@gmail.com>
2016-09-04 12:09:12 -0700

    make sure config file is not named @MOD_ID@

--- src/main/java/com/lazerzes/anvils/library/MiscLib.java
+++ src/main/java/com/lazerzes/anvils/library/MiscLib.java
@@ -3,7 +3,7 @@ package com.lazerzes.anvils.library;
 
 public class MiscLib {
 
-    public static final String MOD_ID = "@MOD_ID@";
+    public static final String MOD_ID = "anvils"; // in order for the config file to NOT be named '@MOD_ID@', i had to change this
     public static final String MOD_NAME = "Anvil Enchantments";
     public static final String MOD_VERSION = "@MOD_VERSION@";
 
