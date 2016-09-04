22ae1781d5f982de3eba681c78cea2e61226e812
Sam <samson.bassett@gmail.com>
2016-09-04 11:12:10 -0700

    initial POC for config file based levels

--- src/main/java/com/lazerzes/anvils/AnvilEnchantments.java
+++ src/main/java/com/lazerzes/anvils/AnvilEnchantments.java
@@ -4,12 +4,30 @@ package com.lazerzes.anvils;
 import com.lazerzes.anvils.handler.EventListener;
 import com.lazerzes.anvils.api.AnvilRecipes;
 import com.lazerzes.anvils.library.MiscLib;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.*;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = MiscLib.MOD_ID, name = MiscLib.MOD_NAME, version = MiscLib.MOD_VERSION)
 public class AnvilEnchantments {
+
+
+    private static Configuration config;
+    @EventHandler
+    public void onPreInit(FMLPreInitializationEvent event) {
+        config = new Configuration(event.getSuggestedConfigurationFile());
+        config.load();
+        int super_cheap = 3, cheap = 5, mid = 10, expensive = 15;
+        int minValue = -1, maxValue = 99;
+        String category = MiscLib.MOD_ID;
+        String comment = "Level required to add enchantmet with the item; set -1 to disable completely";
+        AnvilRecipes.Levels.protection = config.getInt("protection", category, cheap, minValue, maxValue, comment);
+        
+        
+        config.save();
+    }
 
     @EventHandler
     public static void init(FMLInitializationEvent event){
--- src/main/java/com/lazerzes/anvils/api/AnvilRecipes.java
+++ src/main/java/com/lazerzes/anvils/api/AnvilRecipes.java
@@ -17,91 +17,148 @@ public class AnvilRecipes {
     private static ArrayList<AnvilRecipe> recipes = new ArrayList<AnvilRecipe>();
     //public static ArrayList<AnvilRecipe> bookHandler = new ArrayList<AnvilRecipe>();
 
+    static int super_cheap = 3, cheap = 5, mid = 10, expensive = 15;
+    public static class Levels{
+        public static int protection = cheap;
+        public static int fireProtection = super_cheap;
+        public static int blastProtection = super_cheap;
+        public static int projProtection = super_cheap;
+        public static int thorns = cheap;
+        public static int frostWalker = mid;
+        public static int featherFalling = cheap;
+        public static int aquaAffinity = cheap;
+        public static int waterBreathing = cheap;
+        public static int depthStrider =  mid;
+        public static int sharpness = cheap;
+        public static int power = super_cheap;
+        public static int smite = super_cheap;
+        public static int arthropods = super_cheap;
+        public static int fireAspect = super_cheap;
+        public static int flame = super_cheap;
+        public static int knockback;
+        public static int punch = super_cheap;
+        public static int fortune = mid;
+        public static int looting = super_cheap;
+        public static int luckOfSea = super_cheap;
+        public static int efficiency = cheap;
+        public static int lure = super_cheap;
+        public static int silkTouch = mid;
+        public static int unbreaking = mid;
+        public static int infinity = expensive;
+        public static int mending = expensive;
+    }
+
     public static void registerVanillaEnchantmentRecipes(){
 
         ItemStack book = new ItemStack(Items.BOOK, 1);
-        int super_cheap = 3, cheap = 5, mid = 10, expensive = 15;
 
         //Protection
-        addRecipe( new AnvilRecipe(book, new ItemStack(Items.IRON_INGOT, 4), Enchantments.PROTECTION, 1, cheap, true));
+        if( Levels.protection >= 0)
+          addRecipe( new AnvilRecipe(book, new ItemStack(Items.IRON_INGOT, 4), Enchantments.PROTECTION, 1, Levels.protection, true));
 
         //Fire Protection
-        addRecipe( new AnvilRecipe(Enchantments.PROTECTION, 1, new ItemStack(Items.MAGMA_CREAM, 3), Enchantments.FIRE_PROTECTION, 1, super_cheap, true));
+        if(Levels.fireProtection >= 0)
+          addRecipe( new AnvilRecipe(Enchantments.PROTECTION, 1, new ItemStack(Items.MAGMA_CREAM, 3), Enchantments.FIRE_PROTECTION, 1, Levels.fireProtection, true));
 
         //Blast Protection
-        addRecipe( new AnvilRecipe(Enchantments.PROTECTION, 1, new ItemStack(Blocks.OBSIDIAN, 2), Enchantments.BLAST_PROTECTION, 1, super_cheap, true));
+        if( Levels.blastProtection >= 0)
+          addRecipe( new AnvilRecipe(Enchantments.PROTECTION, 1, new ItemStack(Blocks.OBSIDIAN, 2), Enchantments.BLAST_PROTECTION, 1, Levels.blastProtection, true));
 
         //Projectile Protection
-        addRecipe( new AnvilRecipe(Enchantments.PROTECTION, 1, new ItemStack(Items.ARROW, 4), Enchantments.PROJECTILE_PROTECTION, 1, super_cheap, true));
+        if(Levels.projProtection >= 0)
+          addRecipe( new AnvilRecipe(Enchantments.PROTECTION, 1, new ItemStack(Items.ARROW, 4), Enchantments.PROJECTILE_PROTECTION, 1, Levels.projProtection, true));
 
         //Thorns
-        addRecipe( new AnvilRecipe(Enchantments.PROTECTION, 1, new ItemStack(Blocks.CACTUS, 6), Enchantments.THORNS, 1, cheap, true));
+        if(Levels.thorns >= 0)
+          addRecipe( new AnvilRecipe(Enchantments.PROTECTION, 1, new ItemStack(Blocks.CACTUS, 6), Enchantments.THORNS, 1, Levels.thorns, true));
 
         //Frost Walker
-        addRecipe( new AnvilRecipe(book, new ItemStack(Blocks.ICE,4), Enchantments.FROST_WALKER, 1, mid, true));
+        if( Levels.frostWalker >= 0)
+          addRecipe( new AnvilRecipe(book, new ItemStack(Blocks.ICE,4), Enchantments.FROST_WALKER, 1, Levels.frostWalker, true));
 
         //Feather Falling
-        addRecipe( new AnvilRecipe(book, new ItemStack(Items.FEATHER, 8), Enchantments.FEATHER_FALLING, 1, cheap, true));
+        if(Levels.featherFalling >= 0)
+          addRecipe( new AnvilRecipe(book, new ItemStack(Items.FEATHER, 8), Enchantments.FEATHER_FALLING, 1, Levels.featherFalling, true));
 
         //Aqua Affinity
-        addRecipe( new AnvilRecipe(book, new ItemStack(Items.POTIONITEM, 1, PotionType.getID(PotionTypes.WATER)), Enchantments.AQUA_AFFINITY, 1, cheap, true));
+        if(Levels.aquaAffinity >= 0)
+          addRecipe( new AnvilRecipe(book, new ItemStack(Items.POTIONITEM, 1, PotionType.getID(PotionTypes.WATER)), Enchantments.AQUA_AFFINITY, 1, Levels.aquaAffinity, true));
 
         //Respiration
-        addRecipe( new AnvilRecipe(Enchantments.AQUA_AFFINITY, 1, new ItemStack(Items.POTIONITEM, 1, PotionType.getID(PotionTypes.WATER_BREATHING)), Enchantments.RESPIRATION, 1, cheap, true));
+        if(Levels.waterBreathing >= 0)
+          addRecipe( new AnvilRecipe(Enchantments.AQUA_AFFINITY, 1, new ItemStack(Items.POTIONITEM, 1, PotionType.getID(PotionTypes.WATER_BREATHING)), Enchantments.RESPIRATION, 1, Levels.waterBreathing, true));
 
         //Depth Strider
-        addRecipe( new AnvilRecipe(Enchantments.AQUA_AFFINITY, 1, new ItemStack(Blocks.ICE, 8), Enchantments.DEPTH_STRIDER, 1, mid, true));
+        if(Levels.depthStrider >= 0)
+          addRecipe( new AnvilRecipe(Enchantments.AQUA_AFFINITY, 1, new ItemStack(Blocks.ICE, 8), Enchantments.DEPTH_STRIDER, 1, Levels.depthStrider, true));
 
         //Sharpness
-        addRecipe( new AnvilRecipe(book, new ItemStack(Items.QUARTZ, 8), Enchantments.SHARPNESS, 1, cheap, true));
+        if(Levels.sharpness >= 0)
+          addRecipe( new AnvilRecipe(book, new ItemStack(Items.QUARTZ, 8), Enchantments.SHARPNESS, 1, Levels.sharpness, true));
 
         //Power
-        addRecipe( new AnvilRecipe(Enchantments.EFFICIENCY, 1, new ItemStack(Items.ARROW, 4), Enchantments.POWER, 1, super_cheap, true));
+        if(Levels.power >= 0)
+          addRecipe( new AnvilRecipe(Enchantments.EFFICIENCY, 1, new ItemStack(Items.ARROW, 4), Enchantments.POWER, 1,  Levels.power, true));
 
         //Smite
-        addRecipe( new AnvilRecipe(Enchantments.SHARPNESS, 1, new ItemStack(Items.SPLASH_POTION, 1, PotionType.getID(PotionTypes.HEALING)), Enchantments.SMITE, 1, super_cheap, true));
+        if(Levels.smite >= 0)
+          addRecipe( new AnvilRecipe(Enchantments.SHARPNESS, 1, new ItemStack(Items.SPLASH_POTION, 1, PotionType.getID(PotionTypes.HEALING)), Enchantments.SMITE, 1, Levels.smite , true));
 
         //Bane of Arthropods
-        addRecipe( new AnvilRecipe(Enchantments.SHARPNESS, 1, new ItemStack(Items.SPIDER_EYE, 3), Enchantments.BANE_OF_ARTHROPODS, 1, super_cheap, true));
+        if(Levels.arthropods >= 0)
+          addRecipe( new AnvilRecipe(Enchantments.SHARPNESS, 1, new ItemStack(Items.SPIDER_EYE, 3), Enchantments.BANE_OF_ARTHROPODS, 1, Levels.arthropods , true));
 
         //Fire Aspect
-        addRecipe( new AnvilRecipe(Enchantments.SHARPNESS, 1, new ItemStack(Items.BLAZE_POWDER, 3), Enchantments.FIRE_ASPECT, 1, super_cheap, true));
+        if(Levels.fireAspect >= 0)
+          addRecipe( new AnvilRecipe(Enchantments.SHARPNESS, 1, new ItemStack(Items.BLAZE_POWDER, 3), Enchantments.FIRE_ASPECT, 1, Levels.fireAspect , true));
 
         //Flame
-        addRecipe( new AnvilRecipe(Enchantments.FIRE_ASPECT, 1, new ItemStack(Items.ARROW, 4), Enchantments.FLAME, 1, super_cheap, true));
+        if(Levels.flame >= 0)
+          addRecipe( new AnvilRecipe(Enchantments.FIRE_ASPECT, 1, new ItemStack(Items.ARROW, 4), Enchantments.FLAME, 1, Levels.flame, true));
 
         //Knockback
-        addRecipe( new AnvilRecipe(book, new ItemStack(Blocks.PISTON, 2), Enchantments.KNOCKBACK, 1, mid, true));
+        if(Levels.knockback >= 0)
+          addRecipe( new AnvilRecipe(book, new ItemStack(Blocks.PISTON, 2), Enchantments.KNOCKBACK, 1, Levels.knockback, true));
 
         //Punch
-        addRecipe( new AnvilRecipe(Enchantments.KNOCKBACK, 1, new ItemStack(Items.ARROW, 4), Enchantments.PUNCH, 1, super_cheap, true));
+        if(Levels.punch >= 0)
+          addRecipe( new AnvilRecipe(Enchantments.KNOCKBACK, 1, new ItemStack(Items.ARROW, 4), Enchantments.PUNCH, 1, Levels.punch , true));
 
         //Fortune
-        addRecipe( new AnvilRecipe(book, new ItemStack(Items.DYE, 16, EnumDyeColor.BLUE.getDyeDamage()), Enchantments.FORTUNE, 1, mid, true));
+        if(Levels.fortune >= 0)
+          addRecipe( new AnvilRecipe(book, new ItemStack(Items.DYE, 16, EnumDyeColor.BLUE.getDyeDamage()), Enchantments.FORTUNE, 1, Levels.fortune , true));
 
         //Looting
-        addRecipe( new AnvilRecipe(Enchantments.FORTUNE, 1, new ItemStack(Items.ROTTEN_FLESH, 8), Enchantments.LOOTING, 1, super_cheap, true));
+        if(Levels.looting >= 0)
+          addRecipe( new AnvilRecipe(Enchantments.FORTUNE, 1, new ItemStack(Items.ROTTEN_FLESH, 8), Enchantments.LOOTING, 1, Levels.looting, true));
 
         //Luck of the Sea
-        addRecipe( new AnvilRecipe(Enchantments.FORTUNE, 1 , new ItemStack(Items.RABBIT_FOOT, 1), Enchantments.LUCK_OF_THE_SEA, 1, super_cheap, true));
+        if(Levels.luckOfSea >= 0)
+          addRecipe( new AnvilRecipe(Enchantments.FORTUNE, 1 , new ItemStack(Items.RABBIT_FOOT, 1), Enchantments.LUCK_OF_THE_SEA, 1, Levels.luckOfSea , true));
 
         //Efficiency
-        addRecipe( new AnvilRecipe(book, new ItemStack(Items.REDSTONE, 18), Enchantments.EFFICIENCY, 1, cheap, true));
+        if(Levels.efficiency >= 0)
+          addRecipe( new AnvilRecipe(book, new ItemStack(Items.REDSTONE, 18), Enchantments.EFFICIENCY, 1, Levels.efficiency , true));
 
         //Lure
-        addRecipe( new AnvilRecipe(Enchantments.EFFICIENCY, 1, new ItemStack(Items.PRISMARINE_CRYSTALS, 3), Enchantments.LURE, 1, super_cheap, true));
+        if(Levels.lure >= 0)
+          addRecipe( new AnvilRecipe(Enchantments.EFFICIENCY, 1, new ItemStack(Items.PRISMARINE_CRYSTALS, 3), Enchantments.LURE, 1, Levels.lure , true));
 
         //Silk Touch
-        addRecipe( new AnvilRecipe(book, new ItemStack(Items.GHAST_TEAR, 3), Enchantments.SILK_TOUCH, 1, mid, true));
+        if(Levels.silkTouch >= 0)
+          addRecipe( new AnvilRecipe(book, new ItemStack(Items.GHAST_TEAR, 3), Enchantments.SILK_TOUCH, 1, Levels.silkTouch, true));
 
         //Unbreaking
-        addRecipe( new AnvilRecipe(book, new ItemStack(Blocks.OBSIDIAN, 3), Enchantments.UNBREAKING, 1, mid, true));
+        if(Levels.unbreaking >= 0)
+          addRecipe( new AnvilRecipe(book, new ItemStack(Blocks.OBSIDIAN, 3), Enchantments.UNBREAKING, 1, Levels.unbreaking, true));
 
         //Infinity
-        addRecipe( new AnvilRecipe(book, new ItemStack(Items.ARROW, 64), Enchantments.INFINITY, 1, expensive, true));
+        if(Levels.infinity >= 0)
+          addRecipe( new AnvilRecipe(book, new ItemStack(Items.ARROW, 64), Enchantments.INFINITY, 1, Levels.infinity, true));
 
         //Mending
-        addRecipe( new AnvilRecipe(book, new ItemStack(Items.GOLDEN_APPLE, 3), Enchantments.MENDING, 1, expensive, true));
+        if(Levels.mending  >= 0)
+          addRecipe( new AnvilRecipe(book, new ItemStack(Items.GOLDEN_APPLE, 3), Enchantments.MENDING, 1, Levels.mending, true));
 
     }
 
