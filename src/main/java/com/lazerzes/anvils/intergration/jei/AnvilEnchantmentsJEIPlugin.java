4e4d540f532364f5a52e545415b3afcc4a905fe1
lazerzes <lazerzes@gmail.com>
2017-03-14 12:46:30 -0400

    Removed unneccessary code

--- src/main/java/com/lazerzes/anvils/intergration/jei/AnvilEnchantmentsJEIPlugin.java
+++ src/main/java/com/lazerzes/anvils/intergration/jei/AnvilEnchantmentsJEIPlugin.java
@@ -2,30 +2,16 @@ package com.lazerzes.anvils.intergration.jei;
 
 import com.lazerzes.anvils.api.AnvilRecipe;
 import com.lazerzes.anvils.api.AnvilRecipes;
-import com.lazerzes.anvils.intergration.jei.subtypes.EnchantedBookSubtypeInterpreter;
-import com.lazerzes.anvils.intergration.jei.subtypes.PotionSubtypeInterpreter;
 import com.sun.istack.internal.NotNull;
 import mezz.jei.api.*;
 import mezz.jei.api.ingredients.IIngredientRegistry;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+
 
 @JEIPlugin
 public class AnvilEnchantmentsJEIPlugin extends BlankModPlugin{
-
-    @Override
-    public void registerItemSubtypes(ISubtypeRegistry subtypeRegistry){
-        subtypeRegistry.registerSubtypeInterpreter(Items.ENCHANTED_BOOK, EnchantedBookSubtypeInterpreter.INSTANCE);
-        subtypeRegistry.registerSubtypeInterpreter(Items.POTIONITEM, PotionSubtypeInterpreter.INSTANCE);
-        subtypeRegistry.registerSubtypeInterpreter(Items.SPLASH_POTION, PotionSubtypeInterpreter.INSTANCE);
-        subtypeRegistry.registerSubtypeInterpreter(Items.LINGERING_POTION, PotionSubtypeInterpreter.INSTANCE);
-    }
-
-
 
     @Override
     public void register(@NotNull IModRegistry registry){
