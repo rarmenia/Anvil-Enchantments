1ffd43fc16e67e8315083ae250578a632db31cbc
lazerzes <lazerzes@gmail.com>
2018-09-21 20:43:16 -0400

    Enchantment Stripping - Issue #19

--- src/main/java/com/lazerzes/anvils/handler/AnvilEventHandler.java
+++ src/main/java/com/lazerzes/anvils/handler/AnvilEventHandler.java
@@ -3,10 +3,18 @@ package com.lazerzes.anvils.handler;
 import com.lazerzes.anvils.AnvilConfig;
 import com.lazerzes.anvils.AnvilEnchantments;
 import com.lazerzes.anvils.api.anvil.AnvilRecipe;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentData;
 import net.minecraft.init.Items;
+import net.minecraft.item.ItemEnchantedBook;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraftforge.event.AnvilUpdateEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.ArrayList;
 
 
 @Mod.EventBusSubscriber
@@ -30,7 +38,33 @@ public class AnvilEventHandler {
 
             if (event.getLeft().isItemEnchanted() && event.getRight().getItem() == Items.WRITABLE_BOOK){
 
-                //TODO : Add Enchantment Stripping : Remove the enchantments from an item and add them to an enchanted book.
+
+                ArrayList<EnchantmentData> enchantmentDatas = new ArrayList<>();
+                NBTTagList tagList = event.getLeft().getEnchantmentTagList();
+                for (int i = 0; i < tagList.tagCount(); i++){
+
+                    NBTTagCompound tagCompound = tagList.getCompoundTagAt(i);
+                    int id = tagCompound.getShort("id");
+                    int lvl = tagCompound.getShort("lvl");
+
+                    Enchantment temp = Enchantment.getEnchantmentByID(id);
+
+                    if(temp != null) {
+                        enchantmentDatas.add(new EnchantmentData(temp, lvl));
+                    }
+
+                }
+
+                ItemStack enchantedBook = new ItemStack(Items.ENCHANTED_BOOK, 1);
+                for(EnchantmentData ench : enchantmentDatas){
+
+                    ItemEnchantedBook.addEnchantment(enchantedBook, ench);
+
+                }
+
+                event.setCost(AnvilConfig.value.costEnchantmentStripping);
+                event.setOutput(enchantedBook);
+
 
             }
 
